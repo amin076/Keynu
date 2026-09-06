daeb63ec4bdeb066b5c6f01a2801516471f272b1
Amin  Nazari <amin.nazari076@gmail.com>
2026-09-07 06:07:35 +1000

    Register governed HTTP connector

--- src/integrations/registerBuiltinIntegrations.ts
+++ src/integrations/registerBuiltinIntegrations.ts
@@ -1,5 +1,6 @@
 import { CliConnector } from "./connectors/CliConnector.js";
 import { FileConnector } from "./connectors/FileConnector.js";
+import { HttpConnector } from "./connectors/HttpConnector.js";
 import { IntegrationHub } from "./IntegrationHub.js";
 import { MelakatIntegrationPack } from "./packs/melakat/MelakatIntegrationPack.js";
 
@@ -7,6 +8,7 @@ export function createBuiltinIntegrationHub(): IntegrationHub {
   const hub = new IntegrationHub();
   hub.connectors.register(new CliConnector());
   hub.connectors.register(new FileConnector());
+  hub.connectors.register(new HttpConnector());
   hub.registerPack(new MelakatIntegrationPack());
   return hub;
 }
